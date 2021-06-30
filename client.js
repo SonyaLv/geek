0013e23461b540ec58e71b7c11d0751a810f82d3
lvshuya <lvshuya@itdeMacBook-Air.local>
2021-06-30 17:13:12 +0800

    add

--- client.js
+++ client.js
@@ -86,6 +86,102 @@ class ResponseParser {
       body: this.bodyParser.content.join(""),
     };
   }
+  receive(string) {
+    for (let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i));
+    }
+  }
+  receiveChar(char) {
+    if (this.current === this.WAITING_STATUS_LINE) {
+      if (char === "\r") {
+        this.current = this.WAITING_STATUS_LINE_END;
+      } else {
+        this.statusLine += char;
+      }
+    } else if (this.current === this.WAITING_STATUS_LINE_END) {
+      if (char === "\n") {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITING_HEADER_NAME) {
+      if (char === ":") {
+        this.current = this.WAITING_HEADER_SPACE;
+      } else if (char === "\r") {
+        this.current = this.WAITING_HEADER_BLOCK_END;
+        if (this.headers["Transfer-Encodeing"] === "chunked") {
+          this.bodyParser = new TrunkedBodyParser();
+        }
+      } else {
+        this.headerName += char;
+      }
+    } else if (this.current === this.WAITING_HEADER_SPACE) {
+      if (char === " ") {
+        this.current = this.WAITING_HEADER_VALUE;
+      }
+    } else if (this.current === WAITING_HEADER_VALUE) {
+      if (char === "\r") {
+        this.current = this.WAITING_HEADER_LINE_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerValue = "";
+        this.headerName = "";
+      } else {
+        this.headerValue += char;
+      }
+    } else if (this.current === this.WAITING_STATUS_LINE_END) {
+      if (char === "\n") {
+        this.current = this.WAITING_HEADER_NAME;
+      } else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+        if (char === "\n") {
+          this.current == this.WAITING_BODY;
+        }
+      } else if (this.current === this.WAITING_BODY) {
+        this.bodyParser.receiveChar(char);
+      }
+    }
+  }
+}
+class TrunckBodyParser {
+  constructor() {
+    this.WAITING_LENGTH = 0;
+    this.WAITING_LENGTH_LINE_END = 1;
+    this.READING_TRUNK = 2;
+    this.WAITING_NEW_LINE = 3;
+    this.WAITING_NEW_LINE_END = 4;
+    this.length = 0;
+    this.content = [];
+    this.isFinished = false;
+    this.current = this.WAITING_LENGTH;
+  }
+  receiveChar(char) {
+    if (this.current === this.WAITING_LENGTH) {
+      if (char === "\r") {
+        if (this.length === 0) {
+          this.isFinished = true;
+        }
+        this.current = this.WAITING_LENGTH_LINE_END;
+      } else {
+        this.length *= 16;
+        this.length += parseInt(char, 16);
+      }
+    } else if (this.current === this.WAITING_LENGTH_LINE_END) {
+      if (this.char == "\n") {
+        this.current = this.READING_TRUNK;
+      }
+    } else if (this.current === this.READING_TRUNK) {
+      this.content.push(char);
+      this.length--;
+      if (this.length === 0) {
+        this.current = this.WAITING_NEW_LINE;
+      }
+    } else if (this.current === this.WAITING_NEW_LINE) {
+      if (char === "\r") {
+        this.current = this.WAITING_NEW_LINE_END;
+      }
+    } else if (this.current === this.WAITING_NEW_LINE_END) {
+      if (char === "\n") {
+        this.current = this.WAITING_LENGTH;
+      }
+    }
+  }
 }
 void (async function () {
   let request = new Request({
